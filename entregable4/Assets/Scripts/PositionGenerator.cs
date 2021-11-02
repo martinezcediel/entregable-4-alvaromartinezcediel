f3fcb275ef09636b7b8113405edeff33e5decb43
Álvaro Martínez-Cediel Algora <alvaro.martinez.algora@gmail.com>
2021-11-02 17:51:28 +0100

    entregable4

---
+++ entregable4/Assets/Scripts/PositionGenerator.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionGenerator : MonoBehaviour
+{
+    public Vector3 RandomPosition;
+    float x;
+    float y;
+    float z;
+    // Start is called before the first frame update
+    void Start()
+    {
+       // x = Random.Range(0, 8);
+       // y = Random.Range(0, 8);
+       // z = Random.Range(0, 8);
+       // RandomPosition = new Vector3(x, y, z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = Random.Range(0, 8);
+        y = Random.Range(0, 8);
+        z = Random.Range(0, 8);
+        RandomPosition = new Vector3(x, y, z);
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+
+            transform.position = RandomPosition;
+        }
+    }
+}
